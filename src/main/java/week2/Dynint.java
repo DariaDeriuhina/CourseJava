7bbbc9dd727098e8b994a803f05623827da48539
DariaDeriuhina <dardin582@gmail.com>
2023-03-30 11:42:20 +0300

    new data type and multiplication

--- src/main/java/week2/Dynint.java
+++ src/main/java/week2/Dynint.java
@@ -1,6 +1,7 @@
 package week2;
 
 public class Dynint {
+    // new data type and multiplication
         public static void main(String[] args) {
             double radius = 4, height = 5;
 //Переменная volume инициализируется динамически во время
