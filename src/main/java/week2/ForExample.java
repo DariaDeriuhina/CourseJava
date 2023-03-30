fa2311598c1cdc60ca21d244e722937452181b8e
DariaDeriuhina <dardin582@gmail.com>
2023-03-30 11:34:57 +0300

    character sequence

---
+++ src/main/java/week2/ForExample.java
@@ -0,0 +1,7 @@
+package week2;
+
+public class ForExample {
+    public static void main(String args[]){
+        for (int i = 10; i>0; i--) System.out.println(i);
+}
+}
---
+++ src/main/java/week2/ForExample2.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class ForExample2 {
+    public static void main(String args[]){
+        int a,b;
+        for (a=6, b=9; a<b; a++, b-- );{
+            System.out.println("a is "+ a);
+            System.out.println("b is "+ b);
+        }
+    }
+}
