ee6f35bd4fa7a86224fbac217266cf75a95c52b3
DariaDeriuhina <dardin582@gmail.com>
2023-03-30 11:02:16 +0300

    character sequence

---
+++ src/main/java/week2/DivisionInt.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class DivisionInt {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+        System.out.println(a / b); //3
+    }
+}
---
+++ src/main/java/week2/Increment.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class Increment {
+    public static void main(String[] args) {
+        int a = 7;
+        System.out.println(a++); //7
+        System.out.println(++a); //9
+    }
+}
