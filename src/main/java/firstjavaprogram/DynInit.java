4e3bcce08b19d9b4eaa9d9d4d1f2e5cb140d6d81
DariaDeriuhina <dardin582@gmail.com>
2023-03-30 10:38:06 +0300

    Initial commit

---
+++ src/main/java/firstjavaprogram/DynInit.java
@@ -0,0 +1,11 @@
+package Weeklecture2;
+
+/*
+первая программма 
+и многострочный коммент
+*/
+class Firstprogram{
+  public static void main(String[] args){
+    System.out.println("Hello, world!");
+  }
+}
