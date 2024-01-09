a6b9a2db9182bdc86a77410a536785f118cf13c4
DariaDeriuhina <dardin582@gmail.com>
2024-01-09 12:49:20 +0200

    foreigner

---
+++ src/main/java/firstjavaprogram/StepikModule3/Foreigner.java
@@ -0,0 +1,10 @@
+package firstjavaprogram.StepikModule3;
+
+import java.util.Scanner;
+
+public class Foreigner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+    }
+}
