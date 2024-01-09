4233cd6fc03d7981f53ddeccce6facb8a53e6da7
DariaDeriuhina <dardin582@gmail.com>
2024-01-09 13:46:13 +0200

    l12

---
+++ src/main/java/firstjavaprogram/StepikModule3/StepikModule3.java
@@ -0,0 +1,17 @@
+package firstjavaprogram.StepikModule3;
+
+import java.util.Scanner;
+
+public class StepikModule3 {
+    final static int PASSWORD = 133976;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int actualPassword = scanner.nextInt();
+
+        if (actualPassword == PASSWORD){
+            System.out.println("Hello, Agent");
+        } else {
+            System.out.println("Access denied");
+        }
+    }
+}
